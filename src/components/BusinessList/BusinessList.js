62c2fa99964a50a476ae2e40bfe17834b907e218
SanderNiesten <sanderniesten@live.nl>
2018-01-15 03:56:18 +0100

    setup for Yelp-API, add fetch polyfill for older browsers

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -5,15 +5,15 @@ import Business from '../Business/Business';
 class BusinessList extends React.Component {
   render() {
     return (
-      <div className="BusinessList" >
+      <div className="BusinessList">
         {
           this.props.businesses.map(business => {
             return <Business business={business} />
           })
-      }
+        }
       </div>
     );
   }
-};
+}
 
 export default BusinessList;
---
+++ src/util/Yelp.js
@@ -0,0 +1,1 @@
+const apiKey = 'WJ53TOZybBWF3IE_DNs6_Bxr9rsnvNf2HcaE-wxx8y08bLSepli91fCJz8sYB49i8jyoZ5QE3_FsVldMycJ1_7S_G5NVAhRVcan77aiZBieY7zG96AnfFgevbRdcWnYx'
